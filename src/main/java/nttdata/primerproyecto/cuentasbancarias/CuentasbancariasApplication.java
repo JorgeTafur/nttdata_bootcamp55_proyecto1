71f56eaf6f99f5667cb5531473d3002c172fbd0c
JorgeTafur <jorgetafurp@gmail.com>
2024-10-04 14:53:27 -0500

    initial commit

---
+++ src/main/java/nttdata/primerproyecto/cuentasbancarias/CuentasbancariasApplication.java
@@ -0,0 +1,15 @@
+package nttdata.primerproyecto.cuentasbancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasbancariasApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(CuentasbancariasApplication.class, args);
+		System.out.println("test");
+	}
+
+}
